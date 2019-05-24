8e2d12fb7676686efd1b048835a9a13c5c13e3a1
truptigaonkar <trups11.trupti@gmail.com>
2019-05-24 12:01:37 +0200

    html and css files added

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const server = require('http').createServer(app);
+const io = require('socket.io').listen(server);
 
 app.get('/', (req, res) => {
-    res.send("Home page");
+    //res.send("Home page");
+    res.sendfile(__dirname + '/public/index.html'); //html file added
 });
+
+app.use(express.static('public')); // include to access public folder with index.html and style.css
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
