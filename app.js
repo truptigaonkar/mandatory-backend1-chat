0932e2335afa72834e5f917e0c30fc0c174df6db
truptigaonkar <trups11.trupti@gmail.com>
2019-05-25 11:14:59 +0200

    Established socket connection and messages sent to all including me

--- app.js
+++ app.js
@@ -11,3 +11,12 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html'); //html file added
 });
 
+// SERVER SIDE: Message form submit function
+io.sockets.on('connection', function(socket){
+    console.log('Establish socket connection....');
+
+    socket.on('send message', function(data){
+        io.sockets.emit('new message', data); //Sending message to all including me
+        //socket.broadcast.emit('new message', data); // Broadcast message to all except me
+    });
+});
