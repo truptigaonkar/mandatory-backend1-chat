794ef5e16348939cce2689305fd004525c07d78c
truptigaonkar <trups11.trupti@gmail.com>
2019-05-25 13:01:40 +0200

    Divided js, css and html and added into public folder

--- app.js
+++ app.js
@@ -6,17 +6,24 @@ const io = require('socket.io').listen(server);
 server.listen(8000);
 console.log('Server running');
 
-app.get('/', (req, res) => {
-    //res.send("Home page");
-    res.sendFile(__dirname + '/index.html'); //html file added
-});
+app.use(express.static("public")); // where you can have client related files i.e
 
-// SERVER SIDE: Message form submit function
+// //To add single html file only without public folder
+// app.get('/', (req, res) => {
+//     //res.send("Home page");
+//     res.sendFile(__dirname + '/index.html'); //html file added
+// });
+
+
+/* ------------------------- SERVER SIDE functionality------------------------- */
+//Message form submit function
 io.sockets.on('connection', function(socket){
     console.log('Establish socket connection....');
+    console.log(socket.id);
 
     socket.on('send message', function(data){
         io.sockets.emit('new message', data); //Sending message to all including me
         //socket.broadcast.emit('new message', data); // Broadcast message to all except me
     });
 });
+/* -------------------------End SERVER SIDE functionality------------------------- */
---
+++ public/chat.js
@@ -0,0 +1,30 @@
+$(function(){
+    let socket = io.connect();
+    let $messageForm = $('#messageForm');
+    let $messageInput = $('#messageInput');
+    let $chatWindow = $('#chatWindow');
+
+    // Message form submit function
+    $messageForm.submit(function(e){
+        e.preventDefault();
+        console.log($messageInput.val());
+        
+        //Input validation
+        if($messageInput.val() == ''){
+            //alert('Enter message');
+            $messageInput.addClass("messageInput__placeholder");
+            $messageInput.attr("placeholder", "You must write something!!!")
+            
+        }else{
+            $messageInput.attr("placeholder", "Enter message here.....")
+            $messageInput.removeClass("messageInput__placeholder")
+            socket.emit('send message', $messageInput.val()); // socket send message
+        }
+        $messageInput.val(''); // empty input after submission 
+    });
+
+    // CLIENT SIDE functionality
+    socket.on('new message', function(data){ 
+        $chatWindow.append(data +"<br />"); // Display the message on chatwindow
+    });
+});
